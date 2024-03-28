0d1bf935252aa689ce6643616b45a890a51a1e5a
Adriana Cividini <121236952+AdrianaCividini@users.noreply.github.com>
2024-03-28 18:24:40 +0100

    Added the date and time.

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,24 @@
+setInterval(() => {
+  let ushuaiaElement = document.querySelector("#ushuaia");
+  let ushuaiaDateElement = ushuaiaElement.querySelector(".date");
+  let ushuaiaTimeElement = ushuaiaElement.querySelector(".time");
+  let ushuaiaTime = moment().tz("America/Argentina/Ushuaia");
 
+  ushuaiaDateElement.innerHTML = ushuaiaTime.format("MMMM Do YYYY");
+
+  ushuaiaTimeElement.innerHTML = ushuaiaTime.format(
+    "h:mm:ss [<small>]A[<small>]"
+  );
+}, 1000);
+
+function updateRomeTime() {
+  let romeElement = document.querySelector("#rome");
+  let romeDateElement = romeElement.querySelector(".date");
+  let romeTimeElement = romeElement.querySelector(".time");
+  let romeTime = moment().tz("Europe/Rome");
+
+  romeDateElement.innerHTML = romeTime.format("MMMM Do YYYY");
+
+  romeTimeElement.innerHTML = romeTime.format("h:mm:ss SS [<small>]A[<small>]");
+}
+setInterval(updateRomeTime, 1000);
